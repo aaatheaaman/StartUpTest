e27d0a709f6ab1f1925ceae46e0d62eb630237af
aaatheaaman <aaatheaaman@gmail.com>
2026-04-17 21:04:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ const allowedOrigins = [
     "http://localhost:3000",
     "http://127.0.0.1:5501",   // ✅ ADD THIS
     "http://localhost:5500",   // optional (Live Server alt port)
-    "https://your-app.onrender.com",
+    "https://startuptest-2.onrender.com",
     "https://energicainfra.com",
     "https://www.energicainfra.com"
 ];
